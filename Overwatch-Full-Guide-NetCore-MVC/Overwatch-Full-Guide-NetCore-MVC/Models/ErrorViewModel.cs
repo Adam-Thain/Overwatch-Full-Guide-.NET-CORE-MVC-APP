2d400136a28183a9ac79315f8a676f8dc1be2902
Adam-Thain <Adam.r.thain@gmail.com>
2019-06-03 16:18:44 +0100

    Project Setup
    Currently Configured the setup for .NetCore 2.1

---
+++ Overwatch-Full-Guide-NetCore-MVC/Overwatch-Full-Guide-NetCore-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Overwatch_Full_Guide_NetCore_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
